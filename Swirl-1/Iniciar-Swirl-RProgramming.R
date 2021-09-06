96388f2271aa0eafc59d562a16ff3c5a89031839
Carlos Guadián <carlosguadian@gmail.com>
2021-09-06 08:57:35 +0200

    Initial Swirl

---
+++ Swirl-1/Iniciar-Swirl-RProgramming.R
@@ -0,0 +1,32 @@
+#Swirl Inicio con RProgramming
+
+#Instalar paquete
+
+install.packages("swirl")
+
+#comprobar que la versión sea posterior a 2.2.21
+
+packageVersion("swirl")
+
+#Cargar swirl
+
+library(swirl)
+
+#Cargar RProgramming
+
+install_from_swirl("R Programming")
+
+#Empezar con Swirl
+
+swirl()
+
+# Para esta primera parte seguir las siguientes lecciones 
+# (mirar otros archivos de la carpeta)
+
+# Basic Building Blocks
+# Workspace and Files
+# Sequences of Numbers
+# Vectors
+# Missing Values
+# Subsetting Vectors
+# Matrices and Data Frames
